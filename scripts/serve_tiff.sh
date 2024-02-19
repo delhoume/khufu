aadcbcbfd65db5910e3ad2169cd214d814c211be
Frédéric Delhoume <delhoume@gmail.com>
2024-02-19 23:51:10 +0100

    Update serve_tiff.sh

--- scripts/serve_tiff.sh
+++ scripts/serve_tiff.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#usage: servetiff.sh <file>.tif
+
 INPUT=$1
 
 KHUFU_FOLDER=/Users/fredericdelhoume/Documents/GitHub/khufu
@@ -10,5 +12,7 @@ BASE_INPUT=$(basename $1 .tif)
 pushd $BASE_FOLDER
 $KHUFU_FOLDER/bin/tiff2khufu $BASE_INPUT > /tmp/khufu_$BASE_INPUT.sed 
 sed -f /tmp/khufu_$BASE_INPUT.sed $KHUFU_FOLDER/khufu.tpl.html > $BASE_INPUT.html
+cp $KHUFU_FOLDER/openseadragon.js .
+open $BASE_INPUT.html
 $KHUFU_FOLDER/bin/khufu
 popd
