cf15e6fea5866eb44761dcf612ccd859b50caa3f
Frédéric Delhoume <delhoume@gmail.com>
2024-02-24 00:18:21 +0100

    tif2khufu handles tifs with full resolution last
    very nice

--- src/tiff2khufu.cpp
+++ src/tiff2khufu.cpp
@@ -41,32 +41,36 @@ main(int argc, char* argv[]) {
         return 1;
     }
     int directories = TIFFNumberOfDirectories(tifin);
-    // full image must be first
-    unsigned int maxlevel;
-    unsigned int imagewidth;   
-    unsigned int imageheight;
-   unsigned int tilewidth;   
-    unsigned int tileheight;
-    TIFFGetField(tifin, TIFFTAG_IMAGEWIDTH, &imagewidth);
-    TIFFGetField(tifin, TIFFTAG_IMAGELENGTH, &imageheight);
-    TIFFGetField(tifin, TIFFTAG_TILEWIDTH, &tilewidth);
-    TIFFGetField(tifin, TIFFTAG_TILELENGTH, &tileheight);
-    unsigned int filetype;
-    TIFFGetFieldDefaulted(tifin, TIFFTAG_SUBFILETYPE, &filetype);
-    if (filetype == FILETYPE_REDUCEDIMAGE) {
-    } else { //  compute levels
-        unsigned int maxdimension = imagewidth;
-        if (imageheight > imagewidth) maxdimension = imageheight;
-        maxlevel = ceil(log(maxdimension)/log(2));
+    // find the one that is not reduced
+    for (unsigned int idx = 0; idx < directories; ++idx) {
+        TIFFSetDirectory(tifin, idx);
+        unsigned int filetype;
+        TIFFGetFieldDefaulted(tifin, TIFFTAG_SUBFILETYPE, &filetype);
+        if (!(filetype & FILETYPE_REDUCEDIMAGE)) {
+            // first found is used
+            unsigned int maxlevel;
+            unsigned int imagewidth;   
+            unsigned int imageheight;
+            unsigned int tilewidth;   
+            unsigned int tileheight;
+            TIFFGetField(tifin, TIFFTAG_IMAGEWIDTH, &imagewidth);
+            TIFFGetField(tifin, TIFFTAG_IMAGELENGTH, &imageheight);
+            TIFFGetField(tifin, TIFFTAG_TILEWIDTH, &tilewidth);
+            TIFFGetField(tifin, TIFFTAG_TILELENGTH, &tileheight);
+            unsigned int maxdimension = imagewidth;
+            if (imageheight > imagewidth) maxdimension = imageheight;
+                maxlevel = ceil(log(maxdimension)/log(2));
+            std::cout << "s/IMAGE/" << name << "/g" << std::endl
+            << "s/WIDTH/" << imagewidth << "/g" << std::endl
+            << "s/HEIGHT/" << imageheight << "/g" << std::endl
+            << "s/TILESIZE/" << tilewidth << "/g" << std::endl
+            << "s/MINLEVEL/" << (maxlevel - directories + 1) << "/g" << std::endl   
+            << "s/MAXLEVEL/" << maxlevel << "/g" << std::endl
+            << "s/DIRFULL/" << idx << "/g" << std::endl;
+            TIFFClose(tifin);
+            return 0;
+        }
     }
-
-    std::cout << "s/IMAGE/" << name << "/g" << std::endl
-    << "s/WIDTH/" << imagewidth << "/g" << std::endl
-    << "s/HEIGHT/" << imageheight << "/g" << std::endl
-    << "s/TILESIZE/" << tilewidth << "/g" << std::endl
-    << "s/MINLEVEL/" << (maxlevel - directories + 1) << "/g" << std::endl   
-     << "s/MAXLEVEL/" << maxlevel << "/g" << std::endl;
-
     TIFFClose(tifin);
     return 0;
 }
