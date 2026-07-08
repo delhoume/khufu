9b9039f7d424158e9c03f32e6ecd8c0a9b501fd0
Frederic Delhoume <delhoume@fr.ibm.com>
2026-07-08 13:12:34 +0200

    Update khufu.cpp

--- src/khufu.cpp
+++ src/khufu.cpp
@@ -456,8 +456,8 @@ static void cb(struct mg_connection *c, int ev, void *ev_data) {
         delete[] data;
     } else {
       // normal web server
-      char rootbuf[64];'
-      mg_snprintf(rootbuf, sizeof(rootbuf), "%s,/openseadragon=/app/openseadragon", s_root_folders);
+      char rootbuf[256];          
+      mg_snprintf(rootbuf, sizeof(rootbuf), "%s,/openseadragon=/app/openseadragon", s_root_folder);
       struct mg_http_serve_opts opts = {.root_dir = rootbuf}; // Serve local dir
       struct mg_http_message *hm = (struct mg_http_message *)ev_data;
       mg_http_serve_dir(c, hm, &opts);
